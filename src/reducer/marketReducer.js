ade569d86ba544ece270de651d73f97c782d74ff
elisha10108 <78849441+elisha10108@users.noreply.github.com>
2021-08-25 11:44:33 +0300

    25/8/11:44

--- src/reducer/marketReducer.js
+++ src/reducer/marketReducer.js
@@ -1,164 +1,79 @@
-// const stateInit = {
-//   carts_ar: [],
-//   name: "moshe",
-//   showCart:false,
-//   flagP:false,
-//   categories_ar: []
-// }
-
-
-// export const marketReducer = async (state= stateInit , action) => {
-
-//     if(action.type=== "SHOW_HIDE_CART" ) {
-//       if(state.showCart === false){
-//         return saveToLocal({...state, showCart:action.flag});
-//       }
-//       else  return  state.showCart = false
-      
-//     }
-
-
-//     else  if(action.type === "UPDATE_THE_CART")  {
-//     return saveToLocal(updateCart({...state ,  action}));
-
-//     }
-  
-//     else  if(action.type=== "UPDATE_FLAG") {
-//         return ({...state,plagP: action.flagprod});
-//      }
-
-
-//     else{
-//       return (localStorage["market"]) ? JSON.parse(localStorage["market"]) : state;
-//     }
-//   }
-
-
-// // const updateCart = async(state,action) => {
-  
-// //   if(state){
-// //     let prodFound = false;
-// //     let temp_ar =  [...state, state.action.item]
-// //     console.log(temp_ar);
-    
-    
-// //   }
-
-// // //   if(temp_ar.length>0){
-// // //   temp_ar.map((item , i) => {
-// // //     if(item._id === action.item._id){
-// // //       if(item.qty >= action.item.count){
-// // //         item.count = action.item.count;
-// // //       }
-// // //       prodFound = true;
-// // //       // אם הקאונט שווה 0 נמחוק את הפריט מהמערך של העגלה
-// // //       if(action.item.count <= 0){
-// // //         temp_ar.splice(i,1);
-// // //       }
-// // //     }
-    
-// // //   })
-// // // }
-
-
-// // //   if(!prodFound) {
-// // //     temp_ar.push(action.item)
-// // //   }
-// // //   return {...state, carts_ar:temp_ar}
-// // //  }
-
-// // }
-
-
-// const updateCart = (state,action) => {
-//   // נבדוק אם המוצר נמצא
-//   // אם המוצר נמצא אנחנו פשוט נעדכן את הקאונט לקאונט שנשלח בשיגור האחרון
-//   // ואם לא אנחנו נוסיף את המוצר לתוך המערך
-//   // אם שקר יבצע פעולה הוספה למערך אם אמת יעדכן את הקאונט
-//   let prodFound = false;
-//   let temp_ar = [...state.carts_ar]
-//   temp_ar.map((item , i) => {
-//     if(item._id == action.item._id){
-//       // בדוק במלאי כדי לעדכן
-    
-//       if(item.qty >= action.item.count){
-//         item.count = action.item.count;   
-//       }
-//       prodFound = true; 
-//       // אם הקאונט שווה 0 נמחוק את הפריט מהמערך של העגלה
-//       if(action.item.count <= 0){
-//         temp_ar.splice(i,1);
-//       }
-//     }
-//   })
-//   if(!prodFound) {
-//     temp_ar.push(action.item) 
-//   }
-//   return {...state, carts_ar:temp_ar}
-
-// }
-
-// const saveToLocal = (stateTOSave) => {
-//   // localStorage.setItem("market", JSON.stringify(stateTOSave));
-  
-//   return stateTOSave;
-// }
-
-
 const stateInit = {
   carts_ar: [],
   name: "moshe",
   showCart:false,
+  flagP:false,
   categories_ar: []
 }
 
 
-export const marketReducer = (state= stateInit , action) => {
-  switch (action.type){
-    case "SHOW_HIDE_CART":
-      // saveToLocal שומר את זה בהתחלה בלוקאל ואז משגר את הסטטיט
-      return saveToLocal({...state, showCart:action.flag})
-    break;
-    case "UPDATE_THE_CART":
-      return saveToLocal(updateCart(state,action));
-    break;
-    default:
-    // check if there localstorage and update itself
-      return (localStorage["market"]) ? JSON.parse(localStorage["market"]) : state;
-    break
-  }
-}
-
-const updateCart = (state,action) => {
-  // נבדוק אם המוצר נמצא
-  // אם המוצר נמצא אנחנו פשוט נעדכן את הקאונט לקאונט שנשלח בשיגור האחרון
-  // ואם לא אנחנו נוסיף את המוצר לתוך המערך
-  // אם שקר יבצע פעולה הוספה למערך אם אמת יעדכן את הקאונט
-  let prodFound = false;
-  let temp_ar = [...state.carts_ar]
-  temp_ar.map((item , i) => {
-    if(item._id === action.item._id){
-      // בדוק במלאי כדי לעדכן
+export const marketReducer = async (state= stateInit , action) => {
     
-      if(item.qty >= action.item.count){
+        if(action.type=== "SHOW_HIDE_CART" ) {
+          if(state.showCart === false){
+            return saveToLocal({...state, showCart:action.flag});
+          }
+          else  return  state.showCart = false
+    
+        }
+    
+    
+        else  if(action.type === "UPDATE_THE_CART")  {
+        return saveToLocal(updateCart({...state ,  action}));
+    
+        }
+    
+        else  if(action.type=== "UPDATE_FLAG") {
+            return ({...state,plagP: action.flagprod});
+         }
+    
+    
+        else{
+          return (localStorage["market"]) ? JSON.parse(localStorage["market"]) : state;
+        }
+      }
+    
+    
+    
+const updateCart = (state,action) => {
+
+    let prodFound = false;
+    let temp_ar=[];
+    if(state.carts_ar===''){
+     temp_ar = [...state.carts_ar]
+    temp_ar.map((item , i) => {
+        if(item._id === action.item._id){
+            // בדוק במלאי כדי לעדכן
+            if(item.qty >= action.item.count){
+                item.count = action.item.count;
         item.count = action.item.count;   
-      }
+                item.count = action.item.count;
+            }
+            prodFound = true;
       prodFound = true; 
-      // אם הקאונט שווה 0 נמחוק את הפריט מהמערך של העגלה
-      if(action.item.count <= 0){
-        temp_ar.splice(i,1);
-      }
-    }
-  })
-  if(!prodFound) {
+            prodFound = true;
+            // אם הקאונט שווה 0 נמחוק את הפריט מהמערך של העגלה
+            if(action.item.count <= 0){
+                temp_ar.splice(i,1);
+            }
+        }
+        if(!prodFound) {
+            temp_ar.push(action.item)
     temp_ar.push(action.item) 
-  }
-  return {...state, carts_ar:temp_ar}
+            temp_ar.push(action.item)
+        }
+    })
+
 
 }
-
-
+else{
+    console.log(stateInit.carts_ar)
+    stateInit.carts_ar.push(state.action.item,)
+}
+    return {...state, carts_ar:temp_ar}
+}
 const saveToLocal = (stateTOSave) => {
-  localStorage.setItem("market", JSON.stringify(stateTOSave));
-  return stateTOSave;
+    console.log(stateTOSave);
+    localStorage.setItem("market", JSON.stringify(stateTOSave));
+    return stateTOSave;
 }
